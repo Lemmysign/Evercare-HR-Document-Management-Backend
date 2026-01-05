3c8ad9d4dc0166f81f80e56f2871b04f2ef4014e
Lemmysign <innehlemuelux@gmail.com>
2026-01-05 07:38:30 +0100

    Commit one

--- src/main/java/com/hrplatform/dto/request/StaffValidationRequest.java
+++ src/main/java/com/hrplatform/dto/request/StaffValidationRequest.java
@@ -2,6 +2,7 @@ package com.hrplatform.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,5 +19,9 @@ public class StaffValidationRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@evercare\\.ng$",
+            message = "must be an Evercare email (@evercare.ng)"
+    )
     private String email;
 }
