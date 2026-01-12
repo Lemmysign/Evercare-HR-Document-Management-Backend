d6030b80eea652cd5ec7d7208e72403b0c1a4a0b
Lemmysign <innehlemuelux@gmail.com>
2026-01-12 08:59:23 +0100

    Commit one

---
+++ src/main/java/com/hrplatform/dto/request/EmailRequest.java
@@ -0,0 +1,32 @@
+package com.hrplatform.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailRequest {
+
+    @NotBlank(message = "Recipient email is required")
+    @Email(message = "Invalid email format")
+    private String to;
+
+    @NotBlank(message = "Subject is required")
+    private String subject;
+
+    @NotBlank(message = "Email body is required")
+    private String body;
+
+    @Builder.Default
+    private boolean html = false;
+
+    private List<String> cc;
+}
--- src/main/java/com/hrplatform/service/EmailService.java
+++ src/main/java/com/hrplatform/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.hrplatform.service;
 
+import com.hrplatform.dto.request.EmailRequest;
+
+import java.util.concurrent.CompletableFuture;
+
 public interface EmailService {
 
     void sendPasswordResetEmail(String toEmail, String fullName, String resetLink);
@@ -7,4 +11,22 @@ public interface EmailService {
     void sendPasswordResetConfirmationEmail(String toEmail, String fullName);
 
     void sendWelcomeEmail(String toEmail, String fullName, String temporaryPassword);
+
+    CompletableFuture<Boolean> sendOtpEmail(String toEmail, String staffName, String otpCode);
+
+    CompletableFuture<Boolean> sendEmail(EmailRequest request);
+
+    CompletableFuture<Boolean> sendSimpleEmail(String to, String subject, String text);
+
+    EmailStats getEmailStats();
+
+    record EmailStats(
+            long totalSent,
+            long totalFailed,
+            long pendingQueue,
+            double successRate
+    ) {}
+
+
+
 }
